cf3cfc50086e661c5e0c1a59521e775d7a3acee8
himanshubora7 <himanshubora972@gmail.com>
2025-09-13 21:57:28 +0530

    arraylist

---
+++ ArryList.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+// ArrayList
+// dynamic array 
+
+public class ArryList {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+
+        // add an element to the list
+
+        list1.add(2);
+        list1.add(21);
+        System.out.println(list1);
+
+        // access element based on index
+        int x = list1.get(0);
+        System.out.println(x);
+
+        //
+
+    }
+}
